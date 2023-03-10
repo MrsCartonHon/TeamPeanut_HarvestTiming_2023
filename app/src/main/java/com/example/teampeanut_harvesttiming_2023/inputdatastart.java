e37113e9f4b52ca76944b22bb31e40621691b717
ddang175 <ddanton175@gmail.com>
2023-03-09 18:57:24 -0600

    I FIXED THE CRASHING LETS GOOOOOOOOOOO

--- app/src/main/java/com/example/teampeanut_harvesttiming_2023/inputdatastart.java
+++ app/src/main/java/com/example/teampeanut_harvesttiming_2023/inputdatastart.java
@@ -11,19 +11,18 @@ import com.example.teampeanut_harvesttiming_2023.databinding.ActivityInputdatast
 public class inputdatastart extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
-    private ActivityInputdatastartBinding binding;
-    Spinner cropSpinner=findViewById(R.id.crop_dropdown);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityInputdatastartBinding.inflate(getLayoutInflater());
+        com.example.teampeanut_harvesttiming_2023.databinding.ActivityInputdatastartBinding binding = ActivityInputdatastartBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
-
-        ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(this, R.array.crops, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        Spinner cropSpinner = (Spinner) findViewById(R.id.crop_dropdown_menu);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.crops_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         cropSpinner.setAdapter(adapter);
     }
 }
