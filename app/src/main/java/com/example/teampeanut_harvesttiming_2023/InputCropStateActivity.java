c54d59bdaf37f1ae3c268b95fa83a96b869f8b5d
SchmidtNikolas <jklulhehexd2005@gmail.com>
2023-03-29 15:18:36 -0500

    got rid of 2 fragments, to prepare to make them into activities.

---
+++ app/src/main/java/com/example/teampeanut_harvesttiming_2023/InputCropStateActivity.java
@@ -0,0 +1,75 @@
+package com.example.teampeanut_harvesttiming_2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class InputCropStateActivity extends AppCompatActivity  {
+    // One Button
+    Button BSelectImage;
+
+    // One Preview Image
+    ImageView IVPreviewImage;
+
+    // constant to compare
+    // the activity result code
+    int SELECT_PICTURE = 200;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // register the UI widgets with their appropriate IDs
+        BSelectImage = findViewById(R.id.SelectImageButton);
+        IVPreviewImage = findViewById(R.id.CropImage);
+
+        // handle the Choose Image button to trigger
+        // the image chooser function
+        BSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageChooser();
+            }
+        });
+    }
+
+    // this function is triggered when
+    // the Select Image Button is clicked
+    void imageChooser() {
+
+        // create an instance of the
+        // intent of the type image
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+
+    // this function is triggered when user
+    // selects the image from the imageChooser
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url of the image from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // update the preview image in the layout
+                    IVPreviewImage.setImageURI(selectedImageUri);
+                }
+            }
+        }
+    }
+}
