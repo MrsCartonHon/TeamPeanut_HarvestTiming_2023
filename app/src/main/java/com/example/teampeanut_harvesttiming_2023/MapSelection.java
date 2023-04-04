df3c4d41a95ae5f8f3bdf0ff95876c7250662baa
ddang175 <ddanton175@gmail.com>
2023-04-04 15:12:15 -0500

    Menu added with woking buttons with screens we have

--- app/src/main/java/com/example/teampeanut_harvesttiming_2023/MapSelection.java
+++ app/src/main/java/com/example/teampeanut_harvesttiming_2023/MapSelection.java
@@ -2,13 +2,32 @@ package com.example.teampeanut_harvesttiming_2023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MapSelection extends AppCompatActivity {
 
+        private Button toMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_selection);
+
+        toMenu = (Button)findViewById(R.id.next);
+        toMenu.setOnClickListener(new View.OnClickListener()
+        {
+
+
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(getApplicationContext(), Menu.class));
+           /* if you want to finish the first activity then just call
+            finish(); */
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/teampeanut_harvesttiming_2023/Menu.java
@@ -0,0 +1,62 @@
+package com.example.teampeanut_harvesttiming_2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Menu extends AppCompatActivity {
+
+        private Button toNewHarvest, openWeather, openMonitor, openUpdateCropState;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        toNewHarvest = (Button)findViewById(R.id.newHarvest);
+        toNewHarvest.setOnClickListener(new View.OnClickListener()
+        {
+
+
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(getApplicationContext(), StartSelect.class));
+           /* if you want to finish the first activity then just call
+            finish(); */
+
+            }
+        });
+        openWeather = (Button)findViewById(R.id.toWeather);
+        openWeather.setOnClickListener(new View.OnClickListener()
+        {
+
+
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(getApplicationContext(), WeatherActivity.class));
+           /* if you want to finish the first activity then just call
+            finish(); */
+
+            }
+        });
+        openMonitor = (Button)findViewById(R.id.toMonitor);
+        openMonitor.setOnClickListener(new View.OnClickListener()
+        {
+
+
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(getApplicationContext(), MonitorData.class));
+           /* if you want to finish the first activity then just call
+            finish(); */
+
+            }
+        });
+
+    }
+}
